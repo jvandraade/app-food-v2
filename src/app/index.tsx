f83e6a03a958400c621946935fb838cff81f9150
João Vitor Andrade Santos <asauskyboladaum@gmail.com>
2025-07-20 23:33:25 -0300

    identando o código

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,59 +1,50 @@
-import { Text, View, ScrollView } from "react-native";
-import Constants from "expo-constants";
+import { Text, View, ScrollView } from 'react-native';
+import Constants from 'expo-constants';
 
-import { Header } from "../components/header";
-import { Banner } from "../components/banner";
-import { Search } from "../components/search";
-import { Section } from "../components/section";
-import { TrendingFoods } from "../components/trending";
-import { Restaurants } from "../components/restaurants";
-import { RestaurantVerticalList } from "../components/list";
+import { Header } from '../components/header';
+import { Banner } from '../components/banner';
+import { Search } from '../components/search';
+import { Section } from '../components/section';
+import { TrendingFoods } from '../components/trending';
+import { Restaurants } from '../components/restaurants';
+import { RestaurantVerticalList } from '../components/list';
 
 const statusBarHeight = Constants.statusBarHeight;
 
 export default function Index() {
-    return (
-        <ScrollView
-            style={{ flex: 1 }}
-            className="bg-slate-200"
-            showsVerticalScrollIndicator={false}
-        >
-            <View
-                className="w-full px-4"
-                style={{ marginTop: statusBarHeight + 8 }}
-            >
-                <Header />
+  return (
+    <ScrollView style={{ flex: 1 }} className="bg-slate-200" showsVerticalScrollIndicator={false}>
+      <View className="w-full px-4" style={{ marginTop: statusBarHeight + 8 }}>
+        <Header />
+        <Banner />
+        <Search />
+      </View>
 
-                <Banner />
+      <Section
+        name="Comidas em alta"
+        label="Veja mais"
+        action={() => console.log('CLICOU NO VEJA MAIS')}
+        size="text-2xl"
+      />
+      <TrendingFoods />
 
-                <Search />
-            </View>
+      <Section
+        name="Famosos no Jv House of Foods"
+        label="Veja mais"
+        action={() => console.log('CLICOU NO Famosos')}
+        size="text-xl"
+      />
 
-            <Section
-                name="Comidas em alta"
-                label="Veja mais"
-                action={() => console.log("CLICOU NO VEJA MAIS")}
-                size="text-2xl"
-            />
-            <TrendingFoods />
+      <Restaurants />
 
-            <Section
-                name="Famosos no Jv House of Foods"
-                label="Veja mais"
-                action={() => console.log("CLICOU NO Famosos")}
-                size="text-xl"
-            />
+      <Section
+        name="Restaurantes"
+        label="Veja mais"
+        action={() => console.log('CLICOU NO RESTAURANTES')}
+        size="text-xl"
+      />
 
-            <Restaurants />
-
-            <Section
-                name="Restaurantes"
-                label="Veja mais"
-                action={() => console.log("CLICOU NO RESTAURANTES")}
-                size="text-xl"
-            />
-
-            <RestaurantVerticalList />
-        </ScrollView>
-    );
+      <RestaurantVerticalList />
+    </ScrollView>
+  );
 }
